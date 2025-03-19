587281d547d4f19df21f3e58c1beb37521e732b4
randale-porciuncula <onedaleporciuncula@gmail.com>
2025-03-19 16:13:47 +0800

    finished style

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,14 @@ function App() {
 
   return (
     <Container>
-      <div className="h-fit flex flex-row">
+      <div className="min-h-130 flex flex-col items-center md:items-stretch md:flex-row">
         <img
           src="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/rockcms/2022-10/221019-kanye-west-se-218p-14e0cf.jpg"
           alt="Mr. West's picture."
-          className="w-1/2"
+          className="w-full md:w-1/2 object-cover min-h-full"
         />
-        <div className="bg-green-500 text-amber-300 flex flex-col w-1/2 p-10 justify-center items-center font-big-shoulders text-3xl">
-          <div className="flex flex-col">
+        <div className="w-full md:w-1/2 bg-white text-black flex flex-col md:w-1/2 p-10 justify-center items-center font-big-shoulders text-3xl">
+          <div className="flex flex-col max-w-3/4">
             <span className='text-7xl'>"</span>
             <p className='font-semibold'>{quote}</p>
             <span className="self-end text-7xl mt-7">"</span>
@@ -33,7 +33,7 @@ function App() {
         </div>
       </div>
       <button
-        className="bg-white inline w-fit px-5 py-3 cursor-pointer scale-100 hover:scale-110 transition-all ease-in-out duration-300 rounded-md"
+        className="bg-white inline w-fit px-5 py-3 cursor-pointer scale-100 hover:scale-110 transition-all ease-in-out duration-300 rounded-md hover:font-semibold hover:drop-shadow-[0_0_15px_rgba(255,255,255,0.25)]"
         onClick={fetchData}
       >
         KANYE ALSO SAID
@@ -43,3 +43,6 @@ function App() {
 }
 
 export default App;
+
+
+// adjust height 
--- src/components/container/container.jsx
+++ src/components/container/container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function container({children}) {
   return (
-    <div className='flex max-w-7xl flex-col mx-auto content-center min-h-screen justify-center bg-red-500 items-center gap-4'>
+    <div className='flex max-w-7xl flex-col mx-auto content-center min-h-screen justify-center  items-center gap-10'>
       {children}
     </div>
   )
